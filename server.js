a896dd48fbb503f608a56dfee331b375be5f12ca
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2022-11-23 15:33:35 +0100

    codealong with Jenny successfully completed API with animals done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -11,9 +12,51 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//install the Mongo library
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/animals"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }) //always the same set up
+mongoose.Promise = Promise //always the same set up
+
+const Animal = mongoose.model('Animal', {
+  name: String,
+  age: Number,
+  isFurry: Boolean
+})
+
+
+Animal.deleteMany().then(() => {
+new Animal ({ name: 'Alfons', age: 2, isFurry: true }).save()
+new Animal ({ name: 'Lucy', age: 5, isFurry: true }).save()
+new Animal ({ name: 'Goldy the goldsfish', age: 1, isFurry: false }).save()
+})
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  Animal.find().then(animals => {
+    res.json(animals)
+  })
+});
+
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+  res.status(503).json({ error: 'Service unavailable'})
+}
+})
+
+app.get("/:name", (req, res) => { 
+Animal.findOne({name: req.params.name}).then(animal => {
+  try {
+  if(animal) {
+    res.json(animal)
+  } else {
+    res.status(404).json({error: 'Not found'})
+  }
+} catch (err) {
+  res.status(400).json({error: 'invalid id'})
+}
+})
 });
 
 // Start the server
